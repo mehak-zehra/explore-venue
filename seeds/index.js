c828167f3990e3b159325bd821003af905138823
Mehak Zehra <mehak.rizvi.786@gmail.com>
2021-09-28 15:38:25 -0700

    logging to seed data

--- seeds/index.js
+++ seeds/index.js
@@ -3,9 +3,11 @@ const seeds = require('./seed-venue');
 const sequelize = require('../config/connection');
 
 const createSeedingData = async () => {
+  console.log("running seeds....")
   await sequelize.sync({ force: true });
   await seeds();
-  process.exit(0);
+  console.log("completed.....")
+  // process.exit(0);
 };
 
 createSeedingData();
