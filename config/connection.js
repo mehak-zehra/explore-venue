2fe5fce1f51fdac9d96344c2a9b89ab1c4835176
Kyle Balingit <kylebalingit@gmail.com>
2021-09-29 23:22:33 -0700

    Removed Password

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,11 @@ const Sequelize = require('sequelize');
 // if (process.env.JAWSDB_URL) {
 //   sequelize = new Sequelize(process.env.JAWSDB_URL);
 // } else {
-  const sequelize = new Sequelize('explorevenue_db', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
+const sequelize = new Sequelize('explorevenue_db', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
 //}
 
 module.exports = sequelize;
